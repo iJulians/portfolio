b56e04b3ea4fcbc7ff737141ec44e9d524084bc5
Juliano Silveira <silveirajuliano7991@gmail.com>
2026-04-24 02:08:06 -0300

    remove o cursor

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,7 +1,7 @@
 // src/layout/layout.tsx
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen w-full bg-linear-to-b from-dark-primary to-dark-secondary text-white flex justify-center">
+    <div className="min-h-screen w-full bg-linear-to-b from-dark-primary to-dark-secondary text-white flex justify-center cursor-none">
       {children}
     </div>
   );
